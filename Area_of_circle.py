04c4389928647e1d266985b673ee07b67fb97946
imohib168 <50514728+imohib168@users.noreply.github.com>
2019-05-18 14:32:38 +0500

    Delete Area_of_circle.py

--- Area_of_circle.py
+++
@@ -1,4 +0,0 @@
-print("Find Area of a circle by user given radius")
-radius = float(input("Please enter your radius of Circle: "))
-area = 3.14 * radius ** 2
-print("The area of a Circle is: ",area)
